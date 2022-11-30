3f67ac0c750b67dcf30be2c79a72d3bd76226885
rabbani4u <rabbani.iu31@gmail.com>
2022-11-30 19:27:56 +0600

    Update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,5 @@ ReactDOM.render(<h1>Arrow Function</h1>, document.getElementById("root"));
 
 var numbers = [3, 56, 2, 48, 5];
 
-const newNumber = numbers.map(x => {
-  return x * x;
-});
+const newNumber = numbers.map(x => x * x);
 console.log(newNumber);
